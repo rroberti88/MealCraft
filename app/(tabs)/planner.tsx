39f12b8d318ee869e4f3d21043f8bec2acf8a5f6
rroberti88 <r.roberti@studenti.unisa.it>
2026-05-07 17:22:23 +0200

    base del progetto

---
+++ app/(tabs)/planner.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function TempScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Schermata in costruzione</Text>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,77 +1,12 @@
-import { Ionicons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { useColorScheme } from 'react-native';
+import { Stack } from 'expo-router';
+import { AppProvider } from './context/AppContext';
 
-// Definiamo i colori con tipi chiari 
-const ThemeColors = {
-  light: { tint: '#2f95dc', tabIconDefault: '#ccc' },
-  dark: { tint: '#fff', tabIconDefault: '#9ba1a6' },
-} as const; // 'as const' rende l'oggetto a sola lettura e aiuta TS
-
-export default function TabLayout() {
- 
-  const colorScheme = useColorScheme() ?? 'light';
-  
-  
-  const activeColor = ThemeColors[colorScheme].tint;
-
+export default function RootLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: activeColor,
-        headerShown: true,
-      }}>
-      
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Dashboard',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'stats-chart' : 'stats-chart-outline'} size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="pantry"
-        options={{
-          title: 'Dispensa',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'archive' : 'archive-outline'} size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="recipes"
-        options={{
-          title: 'Ricette',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'restaurant' : 'restaurant-outline'} size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="planner"
-        options={{
-          title: 'Planner',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'calendar' : 'calendar-outline'} size={24} color={color} />
-          ),
-        }}
-      />
-
-      <Tabs.Screen
-        name="shopping"
-        options={{
-          title: 'Spesa',
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons name={focused ? 'cart' : 'cart-outline'} size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+    <AppProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </AppProvider>
   );
 }
